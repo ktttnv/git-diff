f255117b95ec0914ab1cddc77359588b47afcc1c
Ekaterina Tatanova <e.tatanova@mail.ru>
2023-03-25 00:03:52 +0300

    Removed App template layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Git diff
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <main className="App-main"></main>
+      <footer className="App-footer">© 2023</footer>
     </div>
   );
 }
